1f3156e16c9563d02140e292012b1735537c536a
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-01-31 18:33:16 -0600

    support weekly builds?

--- config/common.mk
+++ config/common.mk
@@ -256,7 +256,7 @@ ifndef CM_BUILDTYPE
 endif
 
 # Filter out random types, so it'll reset to UNOFFICIAL
-ifeq ($(filter RELEASE NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
+ifeq ($(filter RELEASE NIGHTLY WEEKLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
     CM_BUILDTYPE :=
 endif
 
